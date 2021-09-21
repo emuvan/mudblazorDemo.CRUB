b4f0ef478b1ba57c90da58130742587d9d78c154
van <van.nguyen35@gmail.com>
2021-09-21 11:33:07 +0100

    Add project files.

---
+++ mudblazorDemo.CRUB/Services/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using mudblazorDemo.CRUB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mudblazorDemo.CRUB.Services
+{
+    interface ICustomerService
+    {
+        List<Customer> GetCustomers();
+        Customer GetCustomerById(int id);
+        void SaveCustomer(Customer customer);
+        void DeleteCustomer(int id);
+    }
+}
