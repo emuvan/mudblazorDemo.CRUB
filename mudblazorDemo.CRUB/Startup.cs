0cfe7f1499f6c64416d2d90ea714ab6be527574b
van <van.nguyen35@gmail.com>
2021-09-21 13:41:03 +0100

    took out the data in the startup.cs

--- mudblazorDemo.CRUB/Startup.cs
+++ mudblazorDemo.CRUB/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MudBlazor.Services;
-using mudblazorDemo.CRUB.Data;
 using mudblazorDemo.CRUB.DbContexts;
 using mudblazorDemo.CRUB.Services;
 using System;
